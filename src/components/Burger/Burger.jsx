51c73d620ee7207f2f3a18fe3882fb78a5dc0243
sami <samyramadan.bts@gmail.com>
2023-01-12 16:53:18 +0200

    make burger builder

---
+++ src/components/Burger/Burger.jsx
@@ -0,0 +1,22 @@
+import classes from "./Burger.module.scss";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const Burger = ({ingredients}) => {
+  const transformedIngredients = Object
+    .keys(ingredients)
+    .map(igKey => {
+      return [...Array(ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      { transformedIngredients }
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+}
+
+export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Burger from "../../components/Burger/Burger";
 
 class BuilderBurger extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
+    const {ingredients} = this.state;
     return (
       <>
-        <div>Burger</div>
+        <Burger ingredients={ingredients} />
         <div>Build Controls</div>
       </>
     );
