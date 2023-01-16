25dea4a0b8230cf27339f11480004959e4c2fba4
sami <samyramadan.bts@gmail.com>
2023-01-16 16:18:46 +0200

    improve project structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./components/Layout/Layout";
+import Layout from "./hoc/Layout/Layout";
 import BuilderBurger from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends React.Component {
--- src/components/UI/Model/Model.jsx
+++ src/components/UI/Model/Model.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from "./Model.module.scss";
 
-const Model = ({children, show, modalClosed}) => {
-  return (
-    <>
-      <Backdrop show={show} clicked={modalClosed} />
-      <div 
-        className={classes.Modal} 
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0'
-        }}>
-        {children}
-      </div>
-    </>
-  )
-}
+class Model extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    const {children, show, modalClosed} = this.props;
+    return (
+      <>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div 
+          className={classes.Modal} 
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}>
+          {children}
+        </div>
+      </>
+    )
+  }
+};
 
 export default Model;
